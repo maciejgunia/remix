a5ab8efe28c4ce95660304732a67ce9a292c6fe0
Maciej Gunia <maciej.gunia@intive.com>
2022-02-23 14:13:30 +0100

    fetch from api

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,20 @@
-import { useEffect, useState } from "react";
+import { useLoaderData } from "remix";
+
+export const loader = async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+
+    return data.results;
+};
 
 export default function Index() {
-    const [name, setName] = useState("Maciek");
-
-    useEffect(() => {}, []);
+    const characters: { image: string; id: string }[] = useLoaderData();
 
     return (
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-            <h1>Welcome to Remix</h1>
-            <ul>
-                <li>
-                    <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-                        15m Quickstart Blog Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-                        Deep Dive Jokes App Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-                        Remix Docs
-                    </a>
-                </li>
-            </ul>
+        <div>
+            {characters.map(({ image, id }) => (
+                <img key={id} src={image} alt="" />
+            ))}
         </div>
     );
 }
